5706f706a199f411942150d4903d898ce74e2974
Marian Friedmann <marian.friedmann@gmail.com>
2016-10-26 11:14:44 +0200

    Add hyper.js config file

---
+++ configs/hyper.js
@@ -0,0 +1,90 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 14,
+
+    // font family with optional fallbacks
+    fontFamily: '"Operator Mono",Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: '#4A90E2',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+
+    // color of the text
+    foregroundColor: '#C9C9CB',
+
+    // terminal background color
+    backgroundColor: 'rgba(35,33,44,1)',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '.cursor-node {z-index: -1}',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '12px 14px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#5F5F5F',
+      red: '#FF7F97',
+      green: '#A6EBA6',
+      yellow: '#FBD970',
+      blue: '#69C9EE',
+      magenta: '#F489F4',
+      cyan: '#81C9CF',
+      white: '#DBDBDB',
+      lightBlack: '#737373',
+      lightRed: '#FFA8BA',
+      lightGreen: '#C5EBC5',
+      lightYellow: '#F9F9A5',
+      lightBlue: '#9DE2FF',
+      lightMagenta: '#FFABFF',
+      lightCyan: '#B0F0F0',
+      lightWhite: '#FFFFFF'
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+};
